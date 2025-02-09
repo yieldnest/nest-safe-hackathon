2b141f185be4109346db54deb8e52ae850baca62
Shawn <shawnfladager@gmail.com>
2025-02-09 18:45:44 +0700

    Fix forward ref error.

--- client/src/components/ui/chat/chat-message-list.tsx
+++ client/src/components/ui/chat/chat-message-list.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { ArrowDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { ArrowDown } from "lucide-react";
+import * as React from "react";
 
 interface ChatMessageListProps extends React.HTMLAttributes<HTMLDivElement> {
     scrollRef: React.RefObject<HTMLDivElement | null>;
@@ -11,24 +11,33 @@ interface ChatMessageListProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const ChatMessageList = React.forwardRef<HTMLDivElement, ChatMessageListProps>(
-    ({ className, children, scrollRef, isAtBottom, scrollToBottom, disableAutoScroll, ...props }) => {
+    (
+        {
+            className,
+            children,
+            scrollRef,
+            isAtBottom,
+            scrollToBottom,
+            disableAutoScroll,
+        },
+        ref
+    ) => {
+        const containerRef = scrollRef || ref;
+
         return (
             <div className="relative w-full h-full">
                 <div
                     className={`flex flex-col w-full h-full p-4 overflow-y-auto ${className}`}
-                    ref={scrollRef}
+                    ref={containerRef}
                     onWheel={disableAutoScroll}
                     onTouchMove={disableAutoScroll}
-                    {...props}
                 >
                     <div className="flex flex-col gap-6">{children}</div>
                 </div>
 
                 {!isAtBottom && (
                     <Button
-                        onClick={() => {
-                            scrollToBottom();
-                        }}
+                        onClick={scrollToBottom}
                         size="icon"
                         variant="outline"
                         className="absolute bottom-2 left-1/2 transform -translate-x-1/2 inline-flex rounded-full shadow-md"
